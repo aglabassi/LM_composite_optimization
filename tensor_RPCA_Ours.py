c1fb63811f35e459a831257182b19ec1223b468a
Abdel Ghani Labassi <alabass1@jhu.edu>
2024-08-05 15:14:40 -0400

    finished implemeting the accelerated gradient computation

--- tensor_RPCA_Ours.py
+++ tensor_RPCA_Ours.py
@@ -9,10 +9,18 @@ Created on Mon Aug  5 12:40:55 2024
 
 import torch
 import tensorly as tl
+from itertools import product
+
 tl.set_backend('pytorch')
 
 def tucker_product_optimized_torch(U1, U2, U3, G):
     return tl.tucker_to_tensor((G, [U1,U2,U3]))
+
+
+def outer_product(vectors):
+    dims = len(vectors)
+    einsum_str = ','.join(chr(97 + i) for i in range(dims)) + '->' + ''.join(chr(97 + i) for i in range(dims))
+    return torch.einsum(einsum_str, *vectors)
 
 def jacobian_u1(G, U1, U2, U3):
     n, r = U1.shape
@@ -26,25 +34,59 @@ def jacobian_u1(G, U1, U2, U3):
             )
     return res
 
-def jacobian_u1_optimized(G, U1, U2, U3):
-    n, r = U1.shape
+def jacobian_u2(G, U1, U2, U3):
+    n, r = U2.shape
+    res = torch.zeros((r, n, n, n, n), dtype=torch.float32, device=G.device)
+    for i_ in range(n):
+        for a_ in range(r):
+            eye_n = torch.eye(n, dtype=torch.float32, device=G.device)
+            eye_r = torch.eye(r, dtype=torch.float32, device=G.device)
+            res[a_, i_] = tucker_product_optimized_torch(
+                torch.outer(eye_n[i_], eye_r[a_]), U1, U3, G
+            )
+    return res
+
+
+def jacobian_u_optimized(G, factors, idx):
+    n, r = factors[idx].shape
     I_n = torch.eye(n, dtype=torch.float32, device=G.device).unsqueeze(2).unsqueeze(3)  # Shape: (n, n, 1, 1)
     I_r = torch.eye(r, dtype=torch.float32, device=G.device).unsqueeze(0).unsqueeze(0)  # Shape: (1, 1, r, r)
 
     outer_product = I_n * I_r  # Shape: (n, n, r, r)
-    outer_product = outer_product.permute(2, 0, 1, 3)  # Shape: (r, n, n, n, n)
+    outer_product = outer_product.permute(2, 0, 1, 3)  # Shape: (r, n, n, n, r)
 
     outer_product_reshaped = outer_product.reshape(n * n * r, r)
 
-    res = tucker_product_optimized_torch(outer_product_reshaped, U2, U3, G)
+    res = tl.tucker_to_tensor((G, [outer_product_reshaped] + factors[:idx] + factors[idx+1:]))
 
     res = res.reshape(r, n, n, n, n)
 
     return res
 
+
+def jacobian_g(G, factors):
+    rs = list(G.shape)
+    rs_range = [ range(0,r) for r in rs]
+    ns = [ f.shape[0] for f in factors ]
+    res = torch.zeros(*rs + ns)
+    
+    for item in product(*rs_range):
+        res[item] = outer_product(list(factor[:,r] for (factor,r) in zip(factors, item)))
+    
+    return res #dimension
+    
+
+import torch
+
+def jacobian_g_optimized(G, factors):
+
+    return torch.einsum('ia,jb,kc->abcijk', *factors)
+
+
+
 # Define dimensions
-n = 10
-r = 5
+n = 4
+r = 2
 
 # Initialize random tensors
 G = torch.rand(r, r, r, dtype=torch.float32)
@@ -60,12 +102,24 @@ U2 = U2.to(device)
 U3 = U3.to(device)
 
 # Compute the Jacobians
-t1 = jacobian_u1(G, U1, U2, U3).to(device)
-t2 = jacobian_u1_optimized(G, U1, U2, U3).to(device)
+t1 = jacobian_u2(G, U1, U2, U3).to(device)
+t2 = jacobian_u_optimized(G, [U1, U2, U3], 1).to(device)
 
 print(t1)
 print("----------")
 print(t2)
 
 # Assert statement to check equality
-assert torch.allclose(t2, t1, atol=1e-6), "The outputs are not equal!"
+assert torch.allclose(t2, t1, atol=1e-12), "The outputs are not equal!"
+
+
+# Compute the Jacobians
+t1 = jacobian_g(G, [U1, U2, U3]).to(device)
+t2 = jacobian_g_optimized(G, [U1, U2, U3]).to(device)
+
+print(t1)
+print("----------")
+print(t2)
+
+# Assert statement to check equality
+assert torch.allclose(t2, t1, atol=1e-12), "The outputs are not equal!"
