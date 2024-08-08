6c3e0f1a73547ead3bbae2c566cca083e00df77d
Abdel Ghani Labassi <alabass1@jhu.edu>
2024-08-08 13:29:26 -0400

    improving the jacobian compute

--- tensor_RPCA_Ours.py
+++ tensor_RPCA_Ours.py
@@ -15,8 +15,19 @@ from tensor_RPCA_Theirs import thre
 tl.set_backend('pytorch')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+def compute_jacobian_c_tractable(G, U1, U2, U3):
 
-def jacobian_c(X, Y, Z, U):
+    jacs = [jacobian_g(G, U1,U2,U3), jacobian_u1(G, U1, U2, U3), jacobian_u2(G, U1, U2, U3) , jacobian_u3(G, U1, U2, U3) ]
+    
+    output = torch.cat(jacs, dim=1 )
+    
+    return output
+    
+
+
+
+def compute_jacobian_c_autograd(X, Y, Z, U):
+    
     def c(G,A1,A2,A3):
         return tl.tucker_to_tensor((G, [A1,A2,A3]))
         #return torch.einsum('ia,jb,kc,abc->ijk', A1, A2, A3, G
@@ -62,36 +73,37 @@ def retrieve_tensors(concatenated_tensor, original_shapes):
 
     split_tensors = torch.split(concatenated_tensor, lengths)
 
-    return [split_tensors[i].view(original_shapes[i]).clone().detach().requires_grad_(True) for i in range(len(original_shapes))]
+    return [split_tensors[i].reshape(original_shapes[i]) for i in range(len(original_shapes))]
 
 
 def rpca_ours(X,Y, ranks, z0, n_iter):
-    
-    G0, factors0 = tl.decomposition.tucker(Y - thre(Y, z0, device), rank=ranks)
+    G, factors = tl.decomposition.tucker(Y - thre(Y, z0, device), rank=ranks)
+    n,r = factors[0].shape
+    G, factors = torch.rand(r,r,r), [torch.rand(n,r) for i in range(3)]
     
     errs = []
-    shapes = [ t.shape for t in [G0] + factors0 ]
-    G = G0.clone().detach().requires_grad_(True)
-    factors = [ factor.clone().detach().requires_grad_(True) for factor in factors0 ]
+    shapes = [ t.shape for t in [G] + factors ]
     best_error = torch.sum(torch.abs(X - Y))
     
     for k in range(n_iter):
-        dist_to_sol_emb = torch.norm( tl.tucker_to_tensor((G, factors)) - Y  ).item()
-        h_c_x =  torch.sum(torch.abs( tl.tucker_to_tensor((G, factors)) -  Y)).item()
+        residual = tl.tucker_to_tensor((G, factors)) - Y
+        dist_to_sol_emb = torch.norm( residual ).item()
+        h_c_x =  torch.sum(torch.abs( residual )).item()
+        
         print(dist_to_sol_emb)
         print(h_c_x)
         print(best_error)
         print('---')
         errs.append(torch.abs( h_c_x - best_error ).item())
         
-        jac_c = jacobian_c(G, *factors)
+        jac_c = compute_jacobian_c_tractable(G, *factors)
         
         concatenated = torch.cat([ t.reshape(-1)  for t in [G] + factors ])
-        subgradient = torch.sign( tl.tucker_to_tensor((G, factors)) - Y  ).reshape(-1)
+        subgradient = torch.sign( residual ).reshape(-1)
         
         stepsize = (h_c_x - best_error) / (torch.dot(subgradient, subgradient))
         
-        concatenated = concatenated - stepsize *  (torch.linalg.pinv( jac_c.T@jac_c + 0*torch.eye(jac_c.shape[1]) ) ) @ (jac_c.T @ subgradient )
+        concatenated = concatenated - stepsize *  (torch.linalg.pinv( jac_c.T@jac_c + dist_to_sol_emb*torch.eye(jac_c.shape[1]) ) ) @ (jac_c.T @ subgradient )
         
         tmp = retrieve_tensors(concatenated, shapes)
 
@@ -100,5 +112,116 @@ def rpca_ours(X,Y, ranks, z0, n_iter):
         
         
     return errs
+
+
+
+
+
+def jacobian_g_autograd(G, factors):
+    _, r = factors[0].shape
+    G.clone().detach().requires_grad_(True)
+    factors = [ f.clone().detach().requires_grad_(True) for f in factors]
+    return compute_jacobian_c_autograd(G, *factors)[:, :r**3]
+
     
+def jacobian_u_autograd(G, factors, idx):
+    n,r = factors[idx].shape
+    G.clone().detach().requires_grad_(True)
+    factors = [ f.clone().detach().requires_grad_(True) for f in factors]
+    return compute_jacobian_c_autograd(G, *factors)[:, r**3 + idx*n*r: r**3 + (idx+1)*n*r]
     
+
+
+
+
+
+def jacobian_g(G, U1,U2, U3):
+    return torch.kron(torch.kron(U1, U2), U3)
+
+
+
+
+def jacobian_u1(G, U1, U2, U3):
+    
+    m, r = U1.shape
+    n,_ = U2.shape
+    p,_ = U3.shape
+    
+    res = torch.zeros((m,r, m*n*p), dtype=torch.float32, device=G.device)
+    eye_m = torch.eye(m, dtype=torch.float32, device=G.device)
+    eye_r = torch.eye(r, dtype=torch.float32, device=G.device)
+    for i_ in range(m):
+        for a_ in range(r):
+            E_ia = torch.outer(eye_m[i_], eye_r[a_])
+            res[i_, a_] = (torch.kron(torch.kron(E_ia, U2), U3)@G.reshape(-1))
+            
+    return res.reshape(m*r, m*n*p).permute(1,0)
+
+
+def jacobian_u2(G, U1, U2, U3):
+    
+    m, r = U1.shape
+    n,_ = U2.shape
+    p,_ = U3.shape
+    
+    res = torch.zeros((n, r, m*n*p), dtype=torch.float32, device=G.device)
+    eye_n = torch.eye(n, dtype=torch.float32, device=G.device)
+    eye_r = torch.eye(r, dtype=torch.float32, device=G.device)
+    for i_ in range(n):
+        for a_ in range(r):
+            E_ia = torch.outer(eye_n[i_], eye_r[a_])
+            res[i_, a_] = (torch.kron(torch.kron(U1, E_ia), U3)@G.reshape(-1))
+            
+    return res.reshape(n*r, m*n*p).permute(1,0)
+
+
+def jacobian_u3(G, U1, U2, U3):
+    
+    m, r = U1.shape
+    n,_ = U2.shape
+    p,_ = U3.shape
+    
+    res = torch.zeros((p, r, m*n*p), dtype=torch.float32, device=G.device)
+    eye_n = torch.eye(p, dtype=torch.float32, device=G.device)
+    eye_r = torch.eye(r, dtype=torch.float32, device=G.device)
+    for i_ in range(p):
+        for a_ in range(r):
+            E_ia = torch.outer(eye_n[i_], eye_r[a_])
+            res[i_, a_] = (torch.kron(torch.kron(U1, U2), E_ia)@G.reshape(-1))
+            
+    return res.reshape(p*r, m*n*p).permute(1,0)
+
+n=10
+r=4
+G = torch.rand(r, r, r, requires_grad=True)
+A1 = torch.rand(n, r, requires_grad=True)
+A2 = torch.rand(n, r, requires_grad=True)
+A3 = torch.rand(n, r, requires_grad=True)
+
+J1 = compute_jacobian_c_autograd(G, A1, A2, A3)
+J2 = compute_jacobian_c_tractable(G,A1,A2,A3)
+
+#assert torch.allclose(J1, J2, atol=1e-12), "The outputs are not equal!"
+
+
+t1 = jacobian_g(G, A1, A2, A3).to(device)
+t2 = jacobian_g_autograd(G, [A1, A2, A3]).to(device)
+assert torch.allclose(t1, t2, atol=1e-12), "The outputs are not equal!"
+
+
+
+t1 = jacobian_u1(G, A1, A2, A3).to(device)
+t2 = jacobian_u_autograd(G, [A1, A2, A3], 0).to(device)
+assert torch.allclose(t1, t2, atol=1e-12), "The outputs are not equal!"
+
+
+
+t1 = jacobian_u2(G, A1, A2, A3).to(device)
+t2 = jacobian_u_autograd(G, [A1, A2, A3], 1).to(device)
+assert torch.allclose(t1, t2, atol=1e-12), "The outputs are not equal!"
+
+
+
+t1 = jacobian_u3(G, A1, A2, A3).to(device)
+t2 = jacobian_u_autograd(G, [A1, A2, A3], 2).to(device)
+assert torch.allclose(t1, t2, atol=1e-12), "The outputs are not equal!"
