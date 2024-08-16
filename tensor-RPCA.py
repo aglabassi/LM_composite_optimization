b912b6623eba7a8a6be33e55b9d595a15e592ad0
Abdel Ghani Labassi <alabass1@jhu.edu>
2024-08-16 17:17:58 -0400

    implemented overparameterized regime and normalize error by norm of X*

--- tensor-RPCA.py
+++ tensor-RPCA.py
@@ -11,23 +11,28 @@ import matplotlib.pyplot as plt
 tl.set_backend('pytorch')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def generate_tensor(n, r, kappa):
-    U1 = torch.linalg.svd(torch.rand(n, n, device=device))[0][:, :r]
-    U2 = torch.linalg.svd(torch.rand(n, n, device=device))[0][:, :r]
-    U3 = torch.linalg.svd(torch.rand(n, n, device=device))[0][:, :r]
-    core_G = torch.zeros((r, r, r), device=device)
-    indices = torch.arange(r, device=device)
-    core_G[indices, indices, indices] = kappa ** (-indices / (r - 1))
+def generate_tensor(n, r_true, r, kappa):
     
-    mu = (n/r)*max(torch.max(torch.norm(U1, dim=0, p=float('inf'))),
+    
+    
+    U1 = torch.linalg.svd(torch.rand(n, n, device=device))[0][:, :r_true]
+    U2 = torch.linalg.svd(torch.rand(n, n, device=device))[0][:, :r_true]
+    U3 = torch.linalg.svd(torch.rand(n, n, device=device))[0][:, :r_true]
+    core_G = torch.zeros((r_true, r_true, r_true), device=device)
+    indices = torch.arange(r_true, device=device)
+    core_G[indices, indices, indices] = kappa ** (-indices / (r_true - 1))
+    
+    mu = (n/r_true)*max(torch.max(torch.norm(U1, dim=0, p=float('inf'))),
                    torch.max(torch.norm(U2, dim=0, p=float('inf'))),
                    torch.max(torch.norm(U3, dim=0, p=float('inf'))))
-    
-    return tucker_product_optimized(U1, U2, U3, core_G), mu, core_G, [U1,U2,U3]
+    padding = (0, r - r_true, 0, r - r_true, 0, r - r_true)
+    core_G_extended = torch.nn.functional.pad(core_G, padding)
+    U1_extended = torch.cat((U1, torch.zeros(n, r - r_true)), dim=1)
+    U2_extended = torch.cat((U2, torch.zeros(n, r - r_true)), dim=1)
+    U3_extended = torch.cat((U3, torch.zeros(n, r - r_true)), dim=1)
+    print(core_G_extended)
+    return tl.tucker_to_tensor((core_G, [U1, U2, U3])), mu, core_G_extended, [U1_extended,U2_extended,U3_extended]
 
-
-def tucker_product_optimized(U1, U2, U3, G):
-    return tl.tucker_to_tensor((G, [U1,U2,U3]))
 
 def matrixise(T, mode):
     return tl.unfold(T, mode)
@@ -52,19 +57,20 @@ def min_singular_value(matrix, eps=1e-5):
 
 
 
-n_iter = 1000
+n_iter = 500
 stepsize = 0.4
 
-spectral_init = False
-radius_init = 1
+spectral_init = True
+radius_init = 2
 n = m = p = 20
-r = r_true = 2
+r_true = 2
+r = 6
 kappa = 5
 
 #Theorem 1
 decay_constant = 1-0.45*stepsize #rho in paper
 
-T_true, mu, G, factors = generate_tensor(n, r_true, kappa)
+T_true, mu, G, factors = generate_tensor(n, r_true, r, kappa)
 treshold_0 = 1.5*T_true.abs().max().item() #zeta_0 in paper
 treshold_1 = (2*torch.sqrt(mu*r/n))**3*min( min_singular_value(matrixise(T_true, 0)),
                                         min_singular_value(matrixise(T_true, 1)),
@@ -81,14 +87,14 @@ errs_ours = rpca_ours(G, factors, T_true, T_true_corr, [r,r,r], treshold_0, n_it
 
 
 plt.figure(figsize=(8, 5))
-plt.plot(torch.tensor(errs)/errs[0], label='err_theirs')
-plt.plot(torch.tensor(errs_ours)/errs_ours[0], label='err_ours')
+plt.plot(torch.tensor(errs)/torch.norm(T_true), label='err_theirs')
+plt.plot(torch.tensor(errs_ours)/torch.norm(T_true), label='err_ours')
 
 # Add labels and title
 plt.xlabel('iteration')
 plt.ylabel('error')
 plt.yscale('log')
-plt.title(f'\| c(xt) - c(x*) \|_2 / \| c(x0) - c(x*) \|_2 , spectral init = {spectral_init}')
+plt.title(f'\| c(xt) - c(x*) \|_2 / \| c(x*) \|_2 , spectral init = {spectral_init}')
 plt.legend()
 
 # Show the plot
