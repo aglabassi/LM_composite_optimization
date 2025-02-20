d51bd7e9af2b81a797fc864fe3a75cdf6cf39643
Abdel Ghani Labassi <alabass1@jhu.edu>
2025-02-20 11:30:25 -0500

    add

--- result_processing_methods.py
+++ result_processing_methods.py
@@ -280,7 +280,7 @@ def plot_losses_with_styles(losses, stds, r_true, loss_ord, base_dir, problem, k
             combined_labels.append('')  # Empty label for dummy handle
 
 
-    height = 0.93
+    height = 0.97
 
 
     # Methods legend at upper right
