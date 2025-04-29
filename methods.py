fb1a5c770b18da6d0add90ab3ee09a0bb5848cf6
Abdel Ghani Labassi <alabass1@jhu.edu>
2025-04-29 13:19:32 -0400

    improve

--- methods.py
+++ methods.py
@@ -14,15 +14,12 @@ def subgradient_method(
     n_iter=1000
 ):
     """
-    Levenberg-Marquardt subgradient method.
+    Subgradient method.
 
     Args:
         stepsize_fc: function (k, x) -> stepsize at iteration k.
-        damping_fc: function (k, x) -> damping parameter at iteration k.
         subgradient_fc: function (x) -> element of ∂v(F(x)).
         action_nabla_F_transpose_fc: function (x, v) -> ∇F(x)^T v.
-        levenberg_marquardt_linear_system_solver: 
-            function (x, damping, b) -> solves (∇F(x)^T ∇F(x) + damping * I)g = b.
         x0: starting point.
         n_iter: number of iterations.
 
@@ -92,18 +89,18 @@ def GN_subgradient_method(
     stepsize_fc,
     subgradient_fc,
     action_nabla_F_transpose_fc,
-    levenberg_marquardt_linear_system_solver,
+    gauss_newton_linear_system_solver,
     x0,
     n_iter=1000
 ):
     """
-    Levenberg-Marquardt subgradient method.
+    Gauss-newton subgradient method.
 
     Args:
         stepsize_fc: function (k, x) -> stepsize at iteration k.
         subgradient_fc: function (x) -> element of ∂v(F(x)).
         action_nabla_F_transpose_fc: function (x, v) -> ∇F(x)^T v.
-        levenberg_marquardt_linear_system_solver: 
+        gauss_newton_linear_system_solver: 
             function (x, b) -> solves  (∇F(x)^T ∇F(x)) g = b.
         x0: starting point.
         n_iter: number of iterations.
@@ -118,7 +115,7 @@ def GN_subgradient_method(
         stepsize = stepsize_fc(k, x)
         subgradient = subgradient_fc(x)
         gradient = action_nabla_F_transpose_fc(x, subgradient)
-        preconditioned_gradient = levenberg_marquardt_linear_system_solver(x, gradient)
+        preconditioned_gradient = gauss_newton_linear_system_solver(x, gradient)
         
         x = x - stepsize * preconditioned_gradient
 
